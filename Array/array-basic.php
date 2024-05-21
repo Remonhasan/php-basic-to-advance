3cb6790c0144278786de202fd8435045e52e2987
remonhasanapu <remon.hasan@technovista.com.bd>
2024-05-21 18:11:27 +0600

    added array basic topics and example

---
+++ Array/array-basic.php
@@ -0,0 +1,23 @@
+<?php
+// Define empty array
+$emptylist = [];
+$emptylist = array();
+
+// An array can be initialized with values
+$list = [1, 2];
+$list = array(1, 2);
+// array print by loop
+foreach($list as $value){
+    echo $value;
+    echo " ";
+}
+echo "\n";
+echo "Print element: \n ";
+
+// print individual element 
+echo $list[0];
+echo " ";
+echo $list[1];
+
+
+?>
