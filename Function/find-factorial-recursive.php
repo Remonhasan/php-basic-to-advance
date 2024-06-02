0b49c4e978fa143031f72298610a9197c4aaa7cd
remonhasanapu <remon.hasan@technovista.com.bd>
2024-06-02 15:26:46 +0600

    added functions

---
+++ Function/find-factorial-recursive.php
@@ -0,0 +1,12 @@
+<?php
+echo "Find Factorial (recursive): \n ";
+function factorial($n) {
+    if ($n == 0) {
+        return 1;
+    } else {
+        return $n * factorial($n - 1);
+    }
+}
+
+echo factorial(5);
+?>
